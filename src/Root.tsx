879abd1385a90d8e58875fcad7164fdefb20e539
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-05 15:11:40 -0400

    Estilos añadidos

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,17 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 
 export const Root = () => {
+
+  const { pathname } = useLocation();
+
+  if (pathname === '/') {
+    return <Navigate to="/dashboard" />;
+  }
   
   return (
     <main>
       <Outlet />
-      <Navigate to="/dashboard" />
     </main>
   )
 }
--- src/pages/BearPage.tsx
+++ src/pages/BearPage.tsx
@@ -2,6 +2,10 @@ export const BearPage = () => {
   return (
     <>
       <h1>Contador de Osos</h1>
+      <p>Este es un contador de osos</p>
+
+      <hr />
+      
     </>
   )
 }
