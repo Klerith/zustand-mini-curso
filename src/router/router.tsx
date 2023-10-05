f2e1a70b65dcb19426331afe86ef8dddba4e9809
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-05 12:27:49 -0400

    router colocado

---
+++ src/router/router.tsx
@@ -0,0 +1,36 @@
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+        loader: ({ request }) =>
+          fetch("/api/dashboard.json", {
+            signal: request.signal,
+          }),
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+            loader: redirectIfUser,
+          },
+          {
+            path: "logout",
+            action: logoutUser,
+          },
+        ],
+      },
+    ],
+  },
+]);
