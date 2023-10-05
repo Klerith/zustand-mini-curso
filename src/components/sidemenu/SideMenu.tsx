44af4f46e3905aa925ea83d28b92cbddfa3920dc
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-05 14:36:10 -0400

    SidedMenu - Ancho fijo

--- src/components/sidemenu/SideMenu.tsx
+++ src/components/sidemenu/SideMenu.tsx
@@ -8,21 +8,24 @@ interface MenuItem {
   title: string;
   subTitle: string;
   href: string;
-  icon: IconType;
+  Icon: IconType;
 }
 
 const menuItems: MenuItem[] = [
-  { title: 'Dashboard', subTitle: 'Visualizar data', href: '/dashboard', icon: IoSpeedometerOutline },
-  { title: 'Osos', subTitle: 'Manejador de osos', href: '/dashboard/bears', icon: IoPawOutline },
+  { title: 'Dashboard', subTitle: 'Visualizar data', href: '/dashboard', Icon: IoSpeedometerOutline },
+  { title: 'Osos', subTitle: 'Manejador de osos', href: '/dashboard/bears', Icon: IoPawOutline },
+  { title: 'Persona', subTitle: 'Estado anidado', href: '/dashboard/person', Icon: IoPawOutline },
 ];
 
 
 
 
 export const SideMenu = () => {
+
   return (
-    <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll mr-2">
+    <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-80 left-0 h-screen overflow-y-scroll mr-2">
       <div id="logo" className="my-4 px-6">
+        {/* Title */}
         <h1 className="text-lg md:text-2xl font-bold text-white">
           Zustand
           <span className="text-blue-500 text-xs"> StateManager</span>
@@ -48,29 +51,26 @@ export const SideMenu = () => {
       <nav id="nav" className="w-full px-6">
 
         {
-          menuItems.map( item => (
+          menuItems.map( ( { href, Icon, title, subTitle } ) => (
             <NavLink
-              key={ item.href }
-              to={ item.href }
+              key={ href }
+              to={ href }
               end
             >
               <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-white">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
-                </svg>
-
+                <Icon />
               </div>
               <div className="flex flex-col">
-                <span className="text-lg font-bold leading-5 text-white">{ item.title }</span>
-                <span className="text-sm text-white/50 hidden md:block">{ item.subTitle }</span>
+                <span className="text-lg font-bold leading-5 text-white">{ title }</span>
+                <span className="text-sm text-white/50 hidden md:block">{ subTitle }</span>
               </div>
             </NavLink>
           ) )
         }
 
 
-        
 
+        {/* Logout */}
         <a className="mt-10">
           <div>
             <IoLogOutOutline />
