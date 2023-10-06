e783e26e06b25f1f8aa559b8b5839e2217de8087
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-06 16:51:04 -0400

    WhiteCard props

--- src/components/shared/cards/WhiteCard.tsx
+++ src/components/shared/cards/WhiteCard.tsx
@@ -1,12 +1,19 @@
+import classNames from 'classnames';
+
 interface Props {
   children?: React.ReactNode;
+  centered?: boolean;
+  className?: string;
 }
 
 
-export const WhiteCard = ({ children }: Props) => {
+export const WhiteCard = ( { children, centered, className }: Props ) => {
   return (
-    <div className="bg-white text-center rounded-2xl border shadow-lg p-10">
-      {children}
+    <div className={
+      classNames( 'bg-white rounded-2xl border shadow-lg p-10', className, {
+        'text-center': centered,
+      } ) }>
+      { children }
     </div>
   );
 };
