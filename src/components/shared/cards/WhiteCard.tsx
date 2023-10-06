3389a93f54947eb471ea8973088dd765d8ad4123
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-06 10:13:52 -0400

    Grid con tarjetas

--- src/components/shared/cards/WhiteCard.tsx
+++ src/components/shared/cards/WhiteCard.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export const WhiteCard = ({ children }: Props) => {
   return (
-    <div className="bg-white text-center rounded-2xl border shadow-lg p-10 max-w-xs">
+    <div className="bg-white text-center rounded-2xl border shadow-lg p-10">
       {children}
     </div>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,7 +7,7 @@ export const Dashboard = () => {
       <p>Información general</p>
       <hr />
 
-      <div className="grid">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 
         <WhiteCard>
           <h2>Hola Mundo</h2>
