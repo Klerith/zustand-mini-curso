a10e89c1dacf72b5873058529058510cab2d1d38
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-07 09:43:27 -0400

    Person Page agregada

--- src/pages/01-basic/PersonPage.tsx
+++ src/pages/01-basic/PersonPage.tsx
@@ -44,92 +44,15 @@ export const PersonPage = () => {
                 </div>
               </div>
             </div>
-            <div className="mb-5">
-              <label
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
-                ¿Cuántos invitados traerá?
-              </label>
-              <input
-                type="number"
-                name="guestNumber"
-                id="guestNumber"
-                placeholder="5"
-                min="0"
-                className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
-            </div>
-
-            <div className="-mx-3 flex flex-wrap">
-              <div className="w-full px-3 sm:w-1/2">
-                <div className="mb-5">
-                  <label
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
-                    Fecha de evento
-                  </label>
-                  <input
-                    type="date"
-                    name="eventDate"
-                    id="eventDate"
-                  />
-                </div>
-              </div>
-              <div className="w-full px-3 sm:w-1/2">
-                <div className="mb-5">
-                  <label
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
-                    Hora del evento
-                  </label>
-                  <input
-                    type="time"
-                    name="eventTime"
-                    id="eventTime"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="mb-5">
-              <label className="mb-3 block text-base font-medium text-[#07074D]">
-                ¿Tu también vendrás?
-              </label>
-              <div className="flex items-center space-x-6">
-                <div className="flex items-center">
-                  <input
-                    type="radio"
-                    name="isComing"
-                    id="radioButton1"
-                    className="h-5 w-5"
-                  />
-                  <label
-                    className="pl-3 text-base font-medium text-[#07074D]"
-                  >
-                    Si
-                  </label>
-                </div>
-                <div className="flex items-center">
-                  <input
-                    type="radio"
-                    name="isComing"
-                    id="radioButton2"
-                    className="h-5 w-5"
-                  />
-                  <label
-                    className="pl-3 text-base font-medium text-[#07074D]"
-                  >
-                    No
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <button>
-                Enviar
-              </button>
-            </div>
+  
+            <pre className="bg-gray-200 p-5 rounded-[20px]">
+              {
+                JSON.stringify({
+                  firstName: '',
+                  lastName: ''
+                }, null, 2)
+              }
+            </pre>
           </form>
         </div>
       </WhiteCard>
