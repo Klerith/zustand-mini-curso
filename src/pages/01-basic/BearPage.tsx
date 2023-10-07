8bbb8c84a0602c0768d160950e6d4993da9bf62d
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-07 09:27:07 -0400

    BearPage actualizada

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,3 +1,5 @@
+import { WhiteCard } from '../../components';
+
 export const BearPage = () => {
   return (
     <>
@@ -5,7 +7,39 @@ export const BearPage = () => {
       <p>Manejo de estado simple de Zustand</p>
       <hr />
 
+      <div className="grid grid-cols-3 gap-2">
+        
+        <WhiteCard centered>
+          <h2>Osos Negros</h2>
 
+          <button> +1</button>
+          <span className="text-3xl mx-10"> 0 </span>
+          <button>-1</button>
+
+        </WhiteCard>
+
+        <WhiteCard centered>
+          <h2>Osos Polares</h2>
+
+          <button> +1</button>
+          <span className="text-3xl mx-10"> 0 </span>
+          <button>-1</button>
+
+        </WhiteCard>
+
+        <WhiteCard centered>
+          <h2>Osos Pandas</h2>
+
+          <button> +1</button>
+          <span className="text-3xl mx-10"> 0 </span>
+          <button>-1</button>
+
+        </WhiteCard>
+
+
+
+
+      </div>
 
     </>
   );
