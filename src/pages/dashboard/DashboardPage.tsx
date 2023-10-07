df7ea51642d21fd3e82d7f5e3f385b73955db6e4
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-07 09:59:36 -0400

    Dashboard cards agregadas

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import { IoPawOutline } from 'react-icons/io5';
+import { IoAccessibilityOutline, IoHeartOutline, IoListOutline, IoLockClosedOutline, IoPawOutline } from 'react-icons/io5';
 import { WhiteCard } from '../../components';
 
 export const Dashboard = () => {
@@ -11,10 +11,41 @@ export const Dashboard = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 
         <WhiteCard centered>
-          <IoPawOutline size={50} className="text-indigo-600" />
+          <IoPawOutline size={ 50 } className="text-indigo-600" />
           <h2>Osos</h2>
           <p>Información</p>
         </WhiteCard>
+
+
+        <WhiteCard centered>
+          <IoAccessibilityOutline size={ 50 } className="text-indigo-600" />
+          <h2>Persona</h2>
+          <p>Información</p>
+        </WhiteCard>
+
+
+        <WhiteCard centered>
+          <IoListOutline size={ 50 } className="text-indigo-600" />
+          <h2>Tareas</h2>
+          <p>Información</p>
+        </WhiteCard>
+
+
+        <WhiteCard centered>
+          <IoHeartOutline size={ 50 } className="text-indigo-600" />
+          <h2>Boda</h2>
+          <p>Información</p>
+        </WhiteCard>
+
+
+        <WhiteCard centered>
+          <IoLockClosedOutline size={ 50 } className="text-indigo-600" />
+          <h2>Auth</h2>
+          <p>Información</p>
+        </WhiteCard>
+
+
+
       </div>
 
     </>
