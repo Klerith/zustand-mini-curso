00d9b467ba79f1918c896bcd6b9e77130e09c3ce
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-07 09:49:01 -0400

    Centro en general

--- src/components/shared/cards/WhiteCard.tsx
+++ src/components/shared/cards/WhiteCard.tsx
@@ -12,6 +12,7 @@ export const WhiteCard = ( { children, centered, className }: Props ) => {
     <div className={
       classNames( 'bg-white border rounded-[20px] p-10 shadow-3xl shadow-shadow-500 w-full', className, {
         'text-center': centered,
+        'flex flex-col items-center': centered
       } ) }>
       { children }
     </div>
