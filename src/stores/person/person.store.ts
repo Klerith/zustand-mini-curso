731119463bc12e73262ddffef597d79c14f4e378
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-10 12:41:05 -0400

    No Firebase

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -39,7 +39,7 @@ export const usePersonStore = create<PersonState & Actions>()(
       , {
         name: 'person-storage',
         // storage: customSessionStorage,
-        storage: firebaseStorage,
+        // storage: firebaseStorage,
       } )
   )
 );
