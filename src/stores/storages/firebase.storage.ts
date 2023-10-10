d6c7780599e16713e8604fc3f2697d04b2f0a718
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-10 11:06:53 -0400

    No logs Firebase

--- src/stores/storages/firebase.storage.ts
+++ src/stores/storages/firebase.storage.ts
@@ -19,12 +19,12 @@ const storageApi: StateStorage =  {
   },
 
   setItem: async function ( name: string, value: string ): Promise<void> {
-    const data = await fetch(`${ firebaseUrl }/${ name }.json`, {
+    await fetch(`${ firebaseUrl }/${ name }.json`, {
       method: 'PUT',
       body: value
     }).then( res => res.json());
 
-    console.log(data);
+    // console.count('setItem');
 
     return;
   },
