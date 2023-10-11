481661f5df4beb328072bb8075e74d03b0795c26
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-11 15:36:46 -0400

    Redireccionar al Dashboard si estamos autenticados

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,23 @@
 
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../stores';
 
 export const AuthLayout = () => {
+
+
+  const authStatus = useAuthStore( state => state.status );
+  const checkAuthStatus = useAuthStore( state => state.checkAuthStatus );
+
+  if ( authStatus === 'pending' ) {
+    checkAuthStatus();
+    return <>Loading...</>;
+  }
+
+  if ( authStatus === 'authorized' ) {
+    return <Navigate to='/dashboard' />;
+  }
+
+
   return (
     <div className="bg-gray-100 flex justify-center items-center h-screen">
       <div className="w-1/2 h-screen hidden lg:flex lg:flex-col items-center justify-center bg-indigo-700">
